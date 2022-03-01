6221e83683193039776f5320160ed134691c0753
AlienCoder <37622754+mrclcyn@users.noreply.github.com>
2022-03-01 13:14:11 +0100

    course page done => questions logic not 100% done

--- src/functions/lessonFunctions.js
+++ src/functions/lessonFunctions.js
@@ -1,21 +1,140 @@
-import axios from "axios";
+export const showTitle = (chapter) => {
+  if (chapter.title) {
+    return <h1 className="chapterTitle">{chapter.title}</h1>;
+  }
+};
 
-const lessonsUrl = `http://localhost:4000/lesson/`;
+export const showTable = (chapter) => {
+  if (chapter.table) {
+    return (
+      <table className="chapterTable">
+        {chapter.table.map((row) => {
+          return (
+            <tr>
+              {row.map((e) => {
+                return <td>{e}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </table>
+    );
+  }
+};
 
-// export const fetchAllLessons = async () => {
-//   try {
-//     const { data } = await axios.get(lessonsUrl);
-//     return { __html: data.data };
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+export const showTable_1 = (chapter) => {
+  if (chapter.table_1) {
+    return (
+      <table className="chapterTable">
+        {chapter.table_1.map((row) => {
+          return (
+            <tr>
+              {row.map((e) => {
+                return <td>{e}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </table>
+    );
+  }
+};
 
-// export const dfger = () => {
-//   axios
-//     .get(lessonsUrl)
-//     .then((res) => {
-//       return res.data;
-//     })
-//     .catch((err) => console.log(err));
-// };
+export const showVideo = (chapter) => {
+  if (chapter.youtube) {
+    return (
+      <iframe
+        className="videoPlayer"
+        width="560"
+        height="315"
+        src={chapter.youtube}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    );
+  }
+};
+
+export const showAudioText = (chapter) => {
+  if (chapter.audioText) {
+    return <span className="audioText">{chapter.audioText}</span>;
+  }
+};
+
+export const showAudio = (chapter) => {
+  if (chapter.audio) {
+    return (
+      <iframe
+        className="audioPlayer"
+        width="200"
+        height="50"
+        src={chapter.audio}
+        frameborder="0"
+        title="Audio Player"
+        scrolling="no"
+      ></iframe>
+    );
+  }
+};
+
+export const showQuestionText = (chapter) => {
+  if (chapter.questionText) {
+    return chapter.questionText.map((paragraph) => {
+      return <p className="chapterQuestionText">{paragraph}</p>;
+    });
+  }
+};
+
+export const showQuestions = (chapter) => {
+  if (chapter.questions) {
+    return chapter.questions.map((question) => {
+      if (question.tags == "dropDown") {
+        if (question.answers_1) {
+          return (
+            <form>
+              <select id="questionDropDown" name="questionDropDown">
+                {question.answers.map((answer) => {
+                  return <option value={answer}>{answer}</option>;
+                })}
+              </select>
+              <select id="questionDropDown_1" name="questionDropDown_1">
+                {question.answers_1.map((answer_1) => {
+                  return <option value={answer_1}>{answer_1}</option>;
+                })}
+              </select>
+              <input type="submit" />
+            </form>
+          );
+        } else {
+          return (
+            <form>
+              <select id="questionDropDown" name="questionDropDown">
+                {question.answers.map((answer) => {
+                  return <option value={answer}>{answer}</option>;
+                })}
+              </select>
+            </form>
+          );
+        }
+      } else {
+        <form>
+          {question.answers.map((answer) => {
+            return (
+              <label>
+                {answer}
+                <input
+                  type="radio"
+                  className="multipleChoices"
+                  name={question.tags}
+                  value={answer}
+                />
+              </label>
+            );
+          })}
+        </form>;
+      }
+    });
+  }
+};
--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,55 +1,93 @@
 import { useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { ApiContext } from "../Context";
+import {
+  showTitle,
+  showTable,
+  showTable_1,
+  showVideo,
+  showAudioText,
+  showAudio,
+  showQuestionText,
+  showQuestions,
+} from "../functions/lessonFunctions";
 
 const Lesson = () => {
   const { lessonId, chapterId } = useParams();
-
   const [lessons, setLessons] = useContext(ApiContext);
-  // const [chapterId, setChapterId] = useState(id);
 
-  // const thisChapter = lessons.__html[lessonId].chapters[chapterId];
-
-  const thisLesson = lessons.__html.filter((lesson) => lesson._id == lessonId);
+  const thisLesson = lessons.__html.filter((lesson) => lesson._id === lessonId);
   const thisChapter = thisLesson[0].chapters.filter(
-    (chapter) => chapter._id == chapterId
-  );
+    (chapter) => chapter._id === chapterId
+  )[0];
 
-  // lessons[lessonId]._html.chapters.filter(
-  //   (chapter) => chapter._id == chapterId
-  // );
-  console.log(thisLesson);
   console.log(thisChapter);
-
-  // console.log(thisChapter);
 
   return (
     <>
-      {/* <div>
-        {thisChapter.title && <h2>{thisChapter.title}</h2>}
-        {thisChapter.text[0] && thisChapter.text.map((paragraph) => <p></p>)}
-      </div> */}
-      {/* <h1>Lessons</h1>
-      {lessons.__html.map((lesson) => {
-        return (
-          <div>
-            {lesson.chapters.map((chapter) => {
-              return (
-                <div>
-                  {chapter.text.map((p, index) => {
-                    return (
-                      <div
-                        key={index}
-                        dangerouslySetInnerHTML={{ __html: p }}
-                      />
-                    );
-                  })}
-                </div>
-              );
-            })}
-          </div>
-        );
-      })} */}
+      <div>
+        {showTitle(thisChapter)}
+
+        {/* SHOW TEXT */}
+        {thisChapter.text[0] &&
+          thisChapter.text.map((paragraph) => (
+            <p
+              className="chapterText"
+              dangerouslySetInnerHTML={{ __html: paragraph }}
+            />
+          ))}
+
+        {showAudioText(thisChapter)}
+        {showAudio(thisChapter)}
+        {showTable(thisChapter)}
+
+        {/* SHOW TEXT_1 */}
+        {thisChapter.text_1[0] &&
+          thisChapter.text_1.map((paragraph) => (
+            <p
+              className="chapterText"
+              dangerouslySetInnerHTML={{ __html: paragraph }}
+            />
+          ))}
+
+        {showTable_1(thisChapter)}
+        {showVideo(thisChapter)}
+        {showQuestionText(thisChapter)}
+
+        {/* QUESTION */}
+        {showQuestions(thisChapter)}
+
+        {/* {thisChapter.questions?.text?.map((paragraph) => {
+          return <p className="chapterQuestionText">{paragraph}</p>;
+        })}
+        {thisChapter.questions && thisChapter.questions.tags[0] === "dropDown" ? (
+          thisChapter.questions.answers_1 ? (
+            <form>
+              <select id="questionDropDown" name="questionDropDown">
+                {thisChapter.questions.answers.map((answer) => {
+                  return <option value={answer}>{answer}</option>;
+                })}
+              </select>
+              <select id="questionDropDown_1" name="questionDropDown_1">
+                {thisChapter.questions.answers_1.map((answer_1) => {
+                  return <option value={answer_1}>{answer_1}</option>;
+                })}
+              </select>
+              <input type="submit" />
+            </form>
+          ) : (
+            <form>
+              <select id="questionDropDown" name="questionDropDown">
+                {thisChapter.questions.answers.map((answer) => {
+                  return <option value={answer}>{answer}</option>;
+                })}
+              </select>
+            </form>
+          )
+        ) : (
+          ""
+        )} */}
+      </div>
     </>
   );
 };
